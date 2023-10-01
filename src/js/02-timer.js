c27b94ae458c45cccfe992131897e1c804c591aa
Speedkon <katakononihina@gmail.com>
2023-10-01 18:53:23 +0300

    #2-fix

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,82 +1,71 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const inputTime = document.getElementById('datetime-picker');
-const btStart = document.querySelector('button[data-start]');
-const tDays = document.querySelector('span[data-days]');
-const tHours = document.querySelector('span[data-hours]');
-const tMinutes = document.querySelector('span[data-minutes]');
-const tSeconds = document.querySelector('span[data-seconds]');
+const timerDays = document.querySelector('[data-days]');
+const timerHours = document.querySelector('[data-hours]');
+const timerMin = document.querySelector('[data-minutes]');
+const timerSec = document.querySelector('[data-seconds]');
+const inputEl = document.querySelector('#datetime-picker');
+const startEl = document.querySelector('[data-start]');
 
-let countdownInterval; 
-btStart.setAttribute("disabled", true);
+startEl.disabled = true;
 
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
-    onClose(selectedDates) {
-        const selectedDate = selectedDates[0];
 
-        if (selectedDate > Date.now()) {
-        btStart.removeAttribute('disabled');
+    onClose(selectedDates) {
+        // console.log(selectedDates[0]);
+        if (selectedDates[0] < Date.now()) {
+        alert('Please choose a date in the future')
         } else {
-        Notify.failure('Please choose a date in the future');
+        startEl.disabled = false;
         }
     },
-    };
 
-    const calendar = flatpickr(inputTime, options);
+};
 
-btStart.addEventListener('click', () => {
-const selectedDate = calendar.selectedDates[0];
+startEl.addEventListener('click', onStart);
+    
+function onStart() {
+    startEl.disabled = true;
+    inputEl.disabled = true;
 
-if (selectedDate > Date.now()) {
-    const timeDiff = selectedDate - Date.now();
-        startCountdown(timeDiff);
-    } else {
-        Notify.failure('Please choose a date in the future');
+    const IntId = setInterval(() => {
+    const currentDate = Date.now();
+    const selectetDate = new Date(inputEl.value);
+    const difference = selectetDate - currentDate;
+
+    if (difference < 1000) {
+        clearInterval(IntId);
     }
-});
 
-function startCountdown(ms) {
-    if (countdownInterval) {
-        clearInterval(countdownInterval);
+        const timer = convertMs(difference);
+
+        timerDays.textContent = timer.days.toString().padStart(2, 0);
+        timerHours.textContent = timer.hours.toString().padStart(2, 0);
+        timerMin.textContent = timer.minutes.toString().padStart(2, 0);
+        timerSec.textContent = timer.seconds.toString().padStart(2, 0);
+    }, 1000)
 }
-
-countdownInterval = setInterval(() => {
-    const { days, hours, minutes, seconds } = convertMs(ms);
-
-        tDays.textContent = addLeadingZero(days);
-        tHours.textContent = addLeadingZero(hours);
-        tMinutes.textContent = addLeadingZero(minutes);
-        tSeconds.textContent = addLeadingZero(seconds);
-
-        if (ms <= 0) {
-        clearInterval(countdownInterval);
-        alert("Countdown has finished!");
-        } else {
-        ms -= 1000; 
-        }
-    }, 1000);
-}
-
 function convertMs(ms) {
+    // Number of milliseconds per unit of time
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
     const day = hour * 24;
 
+    // Remaining days
     const days = Math.floor(ms / day);
+    // Remaining hours
     const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
     const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
     return { days, hours, minutes, seconds };
 }
-
-function addLeadingZero(value) {
-    return value < 10 ? `0${value}` : value.toString();
-}
+flatpickr(inputEl, options);
